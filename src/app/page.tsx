36ac846b38c6ae188394c1ea45826fe727e8cf4f
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-02-14 23:02:56 +0300

    setup a data grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@fluentui/react-components";
+import { MyDataGrid } from "./ui/MyDataGrid";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+        <MyDataGrid />
         <Button>Hello Everyone</Button>
     </main>
   );
